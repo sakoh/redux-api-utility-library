f4b5ae037b082340cabdc29da01644ba6046678b
Sako Hartounian <sakohartounian@yahoo.com>
2018-01-17 16:24:28 -0800

    createActionTypeFromKey made one line.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
 import { ActionTypes } from '../models'
 
-export const createActionTypeFromKey = (key: string, type: string) => (
-  `REDUX_API_MIDDLEWARE_${key.toUpperCase()}_${ActionTypes[type]}`
-)
+export const createActionTypeFromKey = (key: string, type: string) => `REDUX_API_MIDDLEWARE_${key.toUpperCase()}_${ActionTypes[type]}`
