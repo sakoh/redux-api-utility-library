e15530360b173b3cb9d569ca88c8b5f898be5b29
Sako Hartounian <sakohartounian@yahoo.com>
2018-01-18 15:54:58 -0800

    still trying to get webpack to work.

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,24 +1,25 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+// var webpack = require('webpack')
+
 module.exports = {
-    entry: path.resolve(__dirname, 'js/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react']
-                }
-            }
-        ]
-    },
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map'
-};
+  entry: path.resolve(__dirname, 'js/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react'],
+        },
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: 'source-map',
+}
