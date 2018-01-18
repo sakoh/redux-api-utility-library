6e5d84f3fbf60fe995edb04b63165cd54f6c2486
Sako Hartounian <sakohartounian@yahoo.com>
2018-01-18 12:57:19 -0800

    Basic boilerplate for integration test set up.

---
+++ examples/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    entry: path.resolve(__dirname, 'js/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
