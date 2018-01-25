dcac7562a4e8490b2f491cbe4753e5cf3daad7ed
Sako Hartounian <sakohartounian@yahoo.com>
2018-01-24 18:07:57 -0800

    Header path in pages corrected.

--- examples/src/pages/Home/index.js
+++ examples/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Header from '../Header'
+import Header from '../../components/Header'
 
 class Home extends Component {
   render () {
--- examples/src/pages/Posts/index.js
+++ examples/src/pages/Posts/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Header from '../Header'
+import Header from '../../components/Header'
 
 class Posts extends Component {
   render () {
